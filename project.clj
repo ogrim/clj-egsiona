8f7bf28739594a8b3c486727b3e45d819d81428e
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2012-03-14 10:13:21 +0100

    Removed unused dependencies and updated clj-obt

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject clj-egsiona "0.1.1"
+(defproject clj-egsiona "0.1.2"
   :description "A Clojure library for detecting locations in Norwegian texts"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-obt "0.3.5"]
+                 [clj-obt "0.3.6"]
                  [org.clojure/java.jdbc "0.0.6"]
-                 [enlive "1.0.0"]
-                 [clj-time "0.3.4"]
                  [clj-http "0.3.2"]])
